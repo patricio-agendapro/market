58ff5c6514b93bf85ccf80f67edca4c829d4cd4c
trillo <sk8unk@gmail.com>
2022-06-15 19:25:25 +0000

    docker permisos escritura logs

--- docker-conf/scripts/docker-entrypoint.sh
+++ docker-conf/scripts/docker-entrypoint.sh
@@ -5,12 +5,13 @@
 
 # START SERVICES
 /etc/init.d/nginx restart
-
+/etc/init.d/php8.1-fpm restart
 
 # START sphinx
 indexer --all --rotate
 searchd
 
+chmod 666 -R /app/storage/logs/
 
 # KEEP CONTAINER ALIVE
 /usr/bin/tail -f /var/log/nginx/access.log
